b5c5c8b210047c661c4f5650fbb1aa7264c9da33
KATHIRVEL28 <95761516+KATHIRVEL28@users.noreply.github.com>
2024-05-31 21:55:19 +0530

    Update DbConnection.java

--- DbConnection.java
+++ DbConnection.java
@@ -3,7 +3,7 @@ package busTicket;
 import java.sql.*;
 public class DbConnection {
 	public static Connection getConnection() throws SQLException{
-		return  DriverManager.getConnection("jdbc:mysql://localhost:3306/BusReservation","root","Kathir@28");
+		return  DriverManager.getConnection("jdbc:mysql://localhost:3306/BusReservation","root","Passwod");
 	}
 
 }
